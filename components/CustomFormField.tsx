3548768947c415e13e73c71309c342f4d7ce7656
magdielian-code <oziomaagaecheta81@gmail.com>
2024-09-09 11:24:15 +0100

    feat: made forms completely reusable.

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -14,11 +14,12 @@ import {
 } from "@/components/ui/form";
 import { FormFieldType } from "./forms/PatientForm";
 import { Control } from "react-hook-form";
+import Image from "next/image";
 
 interface CustomProps {
   control: Control<any>;
   fieldType: FormFieldType;
-  name: string,
+  name: string;
   label?: string;
   placeholder?: string;
   iconSrc?: string;
@@ -28,35 +29,56 @@ interface CustomProps {
   showTimeSelect?: boolean;
   children?: React.ReactNode;
   renderSkeleton?: (field: any) => React.ReactNode;
-
 }
 
-const RenderField = ({ field, props }: {field:any; props: CustomProps}) => {
-    return (
-        <Input 
-            type="text"
-            placeholder="John Doe"
-        />
-    )
-}
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, placeholder, iconAlt, iconSrc, } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt || 'icon'}
+              width={24}
+              height={24}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              {...field}
+              placeholder={placeholder}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+
+    default:
+      break;
+  }
+};
 
 const CustomFormField = (props: CustomProps) => {
-    const { control, fieldType, name, label } = props;
+  const { control, fieldType, name, label } = props;
 
   return (
     <FormField
       control={control}
       name={name}
-      render={({ field }) => 
-      <FormItem>
-        {fieldType !== FormFieldType.CHECKBOX && label && (
+      render={({ field }) => (
+        <FormItem>
+          {fieldType !== FormFieldType.CHECKBOX && label && (
             <FormLabel>{label}</FormLabel>
-        )}
+          )}
 
-        <RenderField field={field} props={props}/>
+          <RenderField field={field} props={props} />
 
-        <FormMessage  className="shad-error"/>
-      </FormItem>}
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
     />
   );
 };
